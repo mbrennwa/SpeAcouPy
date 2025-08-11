c64c97040520e4533a9e51087b6601c3b9bcd3e8
Matthias Brennwald <mbrennwa@gmail.com>
2025-08-11 14:43:00 +0200

    up

--- speacoupy/__init__.py
+++ speacoupy/__init__.py
@@ -2,7 +2,7 @@
 from .domains import Domain, Element, Net, Series, Parallel
 from .electrical import Electrical, Re, Le, Ce
 from .mechanical import Mechanical, Rms, Mms, Cms
-from .acoustic import Acoustic, Ra, Ma, Ca, SealedBox, Port, VentedBox, RadiationPistonWB, RHO0, C0, P0
+from .acoustic import Acoustic, Ra, Ma, Ca, SealedBox, Port, VentedBox, RadiationPiston, RHO0, C0, P0
 from .transformers import AcToMech, MechToElec
 from .driver import DriverMechanicalBranch, Driver
 from .response import ResponseSolver, ResponseResult, omega_logspace
